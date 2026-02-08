bb16e6a16938df9dc382e04bb24ce8c0eb6b6163
sahmedroni <roni.dboy@gmail.com>
2026-02-08 16:50:12 +0600

    feat: Add initial Vite configuration and environment variables for the frontend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://34.41.120.192:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/socket.io': {
-        target: 'http://34.41.120.192:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      }
-    }
-  }
+
 
 })
